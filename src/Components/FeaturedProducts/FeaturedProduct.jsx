95c77b4920c0a0b0b3cabaa8fc5888b87624d4d6
SrilekhaJangam <srilekhajangam@gmail.com>
2026-01-07 22:14:51 +0530

    Initial commit

--- src/Components/FeaturedProducts/FeaturedProduct.jsx
+++ src/Components/FeaturedProducts/FeaturedProduct.jsx
@@ -1,41 +1,64 @@
-// 
-
-import React, { Component } from "react";
 import Slider from "react-slick";
+import productsData from "../Products/productsData";
+import "./FeaturedProducts.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function CenterMode() {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
-  };
-  return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
-  );
+const FeaturedProducts = () => {
+    const filteredProduct = productsData.filter(
+        (product) => product.tag === "featured-product"
+    );
+const settings = {
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 900,
+
+  slidesToShow: 3,
+  slidesToScroll: 1, 
+
+  centerMode:true,
+  centerPadding:"320px",
+  focusOnSelect:true,
+  
+  autoplay: true,
+  autoplaySpeed: 3000,
+
+};
+
+
+    return (
+        <>
+            <section className="featuredproducts">
+                <h2 className="section-title">Featured Products</h2>
+
+
+                <Slider {...settings}>
+                    {
+                    filteredProduct.map((item) => (
+                        <div className="products" key={item.id}>
+                            <div className="card">
+                                <p>{item.title}</p>
+
+                                <img src={item.images[0]} alt={item.title} />
+                                
+                                <p className="price">
+                                    ₹{item.finalPrice.toLocaleString()}
+                                    <span>₹{item.originalPrice.toLocaleString()}</span>
+                                </p>
+
+                            </div>
+                        </div>
+                    ))
+                    }
+                </Slider>
+            </section>
+
+
+
+        </>
+    )
 }
 
-export default CenterMode;
+export default FeaturedProducts;
+
